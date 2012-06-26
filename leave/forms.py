8302e9f62b41ab3a77abcc3af9ee6a15dd262f35
Tomasz Zdanowski <tomasz@mikran.pl>
2012-06-26 16:17:36 +0200

    work in progress

--- leave/forms.py
+++ leave/forms.py
@@ -5,16 +5,16 @@ from django.contrib.admin import widgets
 
 
 YEAR_CHOICES = ('2012', '2013')
-FAVORITE_COLORS_CHOICES = (('1', 'Zaplanowany'),
-                           ('2', 'Zaakceptowany'),
-                           ('3', 'Odrzucony'),
-                           ('4', 'Rezygnuje z urlopu w tym terminie'))
+STATUS_CHOICES = (('PLANNED', 'Zaplanowany'),
+                  ('ACCEPTED', 'Zaakceptowany'),
+                  ('REJECTED', 'Odrzucony'),
+                  ('CANCELLED', 'Rezygnuje z urlopu w tym terminie'))
 
 class LeaveForm(forms.Form):
     #first_day = forms.DateField(initial=datetime.date.today,widget=widgets.AdminDateWidget)
     first_day = forms.DateField(initial=datetime.date.today,widget=SelectDateWidget(years=YEAR_CHOICES))
     last_day = forms.DateField(initial=datetime.date.today,widget=SelectDateWidget(years=YEAR_CHOICES))
-    status = forms.ChoiceField(widget=Select, choices=FAVORITE_COLORS_CHOICES)
+    status = forms.ChoiceField(widget=Select, choices=STATUS_CHOICES)
     message = forms.CharField(widget=forms.Textarea, required=False)
 
     def clean(self):
@@ -23,12 +23,18 @@ class LeaveForm(forms.Form):
         start = cleaned_data.get("first_day")
         end = cleaned_data.get("last_day")
 
-        logging.debug(cleaned_data.get("first_day") < cleaned_data.get("last_day"))
-        logging.debug(end)
+        #logging.debug(cleaned_data.get("first_day") < cleaned_data.get("last_day"))
+        #logging.debug(end)
 
         if start > end:
             raise forms.ValidationError("Nie no bez przesady ! Chcesz urlop od tylu ? Data zakonczenia musi byc pozniej :-)")
 
         # Always return the full collection of cleaned data.
         return cleaned_data
+
+    def isCancelled(self,choice):
+        if choice == 'CANCELLED' : 
+            return True
+        else:
+            return False;
 
